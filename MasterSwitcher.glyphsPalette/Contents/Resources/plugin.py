344fd28c56d7202c21eb1d545884fe86cae15dca
Rainer Scheichelbauer <res@glyphsapp.com>
2020-02-11 03:21:21 +0100

    initial commit

---
+++ MasterSwitcher.glyphsPalette/Contents/Resources/plugin.py
@@ -0,0 +1,81 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Palette Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Palette
+#
+#
+###########################################################################################################
+
+from __future__ import division, print_function, unicode_literals
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class MasterSwitcher (PalettePlugin):
+	
+	dialog = objc.IBOutlet()
+	previousSwitch = objc.IBOutlet()
+	nextSwitch = objc.IBOutlet()
+	italicSwitch = objc.IBOutlet()
+	
+	@objc.python_method
+	def settings(self):
+		self.name = Glyphs.localize({
+			'en': u'Master Switcher',
+			'de': u'Master-Switcher',
+			'es': u'Control de Másteres',
+		})
+		
+		# Load .nib dialog (without .extension)
+		self.loadNib('IBdialog', __file__)
+	
+	# @objc.python_method
+	# def start(self):
+	# 	# Adding a callback for the 'GSUpdateInterface' event
+	# 	Glyphs.addCallback(self.update, UPDATEINTERFACE)
+	#
+	# @objc.python_method
+	# def __del__(self):
+	# 	Glyphs.removeCallback(self.update)
+	
+	# Action triggered by UI
+	@objc.IBAction
+	def switchMaster_( self, sender=None ):
+		font = Glyphs.font
+		if font:
+			# default: next master
+			step = 1
+			
+			# determine which button:
+			if sender is self.previousSwitch:
+				step = -1
+			elif sender is self.italicSwitch:
+				step = int(len(font.masters)/2)
+			
+			# step to master:
+			font.masterIndex = (font.masterIndex+step) % len(font.masters)
+	
+	@objc.python_method
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
+	
+	# Temporary Fix
+	# Sort ID for compatibility with v919:
+	_sortID = 0
+	@objc.python_method
+	def setSortID_(self, id):
+		try:
+			self._sortID = id
+		except Exception as e:
+			self.logToConsole( "setSortID_: %s" % str(e) )
+	
+	@objc.python_method
+	def sortID(self):
+		return self._sortID
+	
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # MasterSwitcher
-Glyphs.app palette for stepping through font masters
+
+This is a [Glyphs.app](http://glyphsapp.com/) palette plug-in  for stepping through font masters. It is very useful if you have to juggle many masters, or need to repeat a task for each master.
+
+![MasterSwitcher](MasterSwitcher.png)
+
+Use the left button to step to the previous master, and the right button to step to the next master. Use the middle button for jumping half the masters. This is useful if one half of your masters is e.g. narrow or italic.
+
+### Installation
+
+1. One-click install *Master Switcher* from *Window > Plugin Manager*
+2. Restart Glyphs.
+
+### Usage Instructions
+
+1. Open the Palette (Cmd-Opt-P).
+2. Click on the buttons in the *Master Switcher* section.
+
+### License
+
+Copyright 2020 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
